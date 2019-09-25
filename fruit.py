ea7dfb14fa6b08c874d492b97ed01af9a34c4b2b
tommaso <tommaso131195@gmail.com>
2019-09-25 16:44:52 +0200

    Set up iterator for defects in fruit

--- fruit.py
+++ fruit.py
@@ -11,13 +11,71 @@ class Fruit:
 	def __init__(self, index, load_path, defects_thresholds=[160]):
 		"""
 		Instantiates the Fruit object
+
+		Parameters
+		----------
+		load_path : str
+			load path of the fruit's shots
+		defects_thresholds : list
+			list of thresholds for labeling
 		"""
 
 		self.index = index
 		self.defects = Fruit.load(load_path, index, defects_thresholds)
 
+		self.shots_tot = len(self.defects)
+		self.defects_tot = sum([len(defects_per_shot) for defects_per_shot in self.defects])
+
+		self.shots_index = next(i for i, defects_list in enumerate(self.defects) if defects_list) if self.defects_tot else 0
+		self.defects_in_shots_index = 0
+		self.defects_index = -1
+
+		# self.defects_analyzed = 0
+		# self.defects_identified = 0
+		
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		"""
+		Return the next defect to be analyzed and updates all the counters
+
+		Returns
+		-------
+		defect : Defect
+			next defect to be analyzed
+		"""
+
+		if self.defects_in_shots_index == len(self.defects[self.shots_index]):
+			self.defects_in_shots_index = 0
+			self.shots_index += next((i for i, v in enumerate(self.defects[self.shots_index+1:]) if v), 0)+1
+			
+		if self.defects_index < self.defects_tot-1:
+			defect = self.defects[self.shots_index][self.defects_in_shots_index]
+			self.defects_in_shots_index += 1
+			self.defects_index += 1
+			return defect
+		else:
+			raise StopIteration
+
+
 	@staticmethod
 	def load(load_path, index, defects_thresholds):
+		"""
+		Loads shots and answers (indices of defects on the fruit)
+
+		Parameters
+		----------
+		load_path : str
+			load path of the fruit's shots
+		defects_thresholds : list
+			list of thresholds for labeling
+
+		Returns
+		-------
+		defects : list
+			list of defects divided in sublists (shots)
+		"""
 
 		name = load_path + "{0}.tiff".format(index)
 		with tifffile.TiffFile(name) as tif:
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fruit import Fruit
 
 f = Fruit(0, "dataset/dataset/")
-print(f.defects)
+
+for d in f:
+	print()
+	print(f.shots_index, f.shots_tot)
+	print(f.defects_index, f.defects_tot)
+	print("###", d.shot_name, d.index, "###")
