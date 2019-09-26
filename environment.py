326c9258a93d96bd74d5ec04bc2a9923058c2bfd
tommaso <tommaso131195@gmail.com>
2019-09-26 09:49:28 +0200

    Added environment sync function

--- environment.py
+++ environment.py
@@ -6,6 +6,7 @@ class Environment:
 	def __init__(self, scope, starting_index=0,
 				load_path="dataset/dataset/", defects_thresholds=[160]):
 
+		self.scope = scope
 		self.load_path = load_path
 		self.defects_thresholds = defects_thresholds
 
@@ -13,14 +14,26 @@ class Environment:
 			self.index = tf.Variable(starting_index-1, dtype=tf.int32,
 									name='index', trainable=False)
 
+	@staticmethod
+	def sync(sess, from_scope, to_scope):
+
+		from_var = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, from_scope)
+		to_var = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, to_scope)
+
+		sess.run(to_var[0].assign(from_var[0]))
+
 	def load_fruit(self, sess):
 
-		index = sess.run(self.index)+1
-		fruit = Fruit(index, self.load_path, self.defects_thresholds)
+		Environment.sync(sess, "global_env", self.scope)
+
+		temp_index = sess.run(self.index)+1
+		fruit = Fruit(temp_index, self.load_path, self.defects_thresholds)
 
 		while not fruit.defects_tot:
-			index += 1
-			fruit = Fruit(index, self.load_path, self.defects_thresholds)
+			temp_index += 1
+			fruit = Fruit(temp_index, self.load_path, self.defects_thresholds)
 
 		self.fruit = fruit
-		sess.run(self.index.assign(index))
+		sess.run(self.index.assign(temp_index))
+		
+		Environment.sync(sess, self.scope, "global_env")
